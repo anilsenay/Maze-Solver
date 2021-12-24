f2b6204f350ad058ccd41bbdaf5b2bf5c35ff844
Beyza Aydoğan <beyzaaydogan@Beyzas-MacBook-Pro.local>
2021-12-24 20:17:45 +0300

    added bfs

--- base.py
+++ base.py
@@ -3,5 +3,33 @@ from create_tree import createTree
 
 
 mazeArray, startNode = createMaze()
-root = createTree(mazeArray, startNode)
-print(root)
+root = createTree(mazeArray, startNode) 
+
+def bfs(root):
+    frontier = []
+    explored = []
+    cost = 0
+    frontier.append(root)
+    i = 0
+    while True:
+        if (len(frontier) == 0):
+            return "Error! Frontier is empty."
+        node = frontier.pop(0)
+        
+        # print(node)
+        explored.append(node.square)
+        cost = cost + node.cost
+        for child in node.children:
+            if (child.isGoal):
+                
+                print(child)
+                cost = cost + child.cost
+                return cost
+            if (child.square in explored):
+                continue
+            frontier.append(child)
+            
+        i = i + 1
+
+result = bfs(root)
+# print(result)
--- create_tree.py
+++ create_tree.py
@@ -1,13 +1,14 @@
 from MazeSquare import MazeSquare
 
 class Node:
-  def __init__(self, cost, isGoal, children):
+  def __init__(self, square, cost, isGoal, children):
+    self.square = square
     self.cost = cost
     self.isGoal = isGoal
     self.children = children
   
   def __str__(self) -> str:
-    return 'cost:{}, isGoal:{}, children:{}\n'.format(self.cost, self.isGoal, self.children)
+    return 'square:{}, cost:{}, isGoal:{}, children:{}\n'.format(self.square, self.cost, self.isGoal, self.children)
 
 COST = {
   "START": 0,
@@ -23,17 +24,17 @@ def createTree(nodeList, startNode: MazeSquare):
   global mazeArray
   mazeArray = nodeList
   root = createNodeWithChilds(startNode)
-  print(root)
+  # print(root)
   return root
 
-def createNodeWithChilds(node: MazeSquare):
-  print(node)
+def createNodeWithChilds(node: MazeSquare): 
+  # print(node)
   stack.append(node)
   nodeChildren = []
 
   if(node.squareType == "GOAL"):
     stack.pop()
-    return Node(COST[node.squareType], False, nodeChildren)
+    return Node(node, COST[node.squareType], False, nodeChildren)
 
   if(not(node.hasEastWall)):
     if(not(mazeArray[(node.x - 1)][(node.y - 1) + 1] in stack)):
@@ -52,4 +53,4 @@ def createNodeWithChilds(node: MazeSquare):
       northSquare = createNodeWithChilds(mazeArray[(node.x - 1) - 1][(node.y - 1)])
       nodeChildren.append(northSquare)
   stack.pop()
-  return Node(COST[node.squareType], False, nodeChildren)
+  return Node(node, COST[node.squareType], False, nodeChildren)

07b884e5663a5388b81e231361e0b21b0817e477
Anıl Şenay <anilsenay3@gmail.com>
2021-12-29 12:04:56 +0300

    Fix maze coordinates

--- create_tree.py
+++ create_tree.py
@@ -43,20 +43,20 @@ def createNodeWithChilds(node: MazeSquare, rootDepth, costSoFar):
     return Node(node, COST[node.squareType], True, nodeChildren, nodeDepth, costSoFar)
 
   if(not(node.hasEastWall)):
-    if(not(mazeArray[(node.x - 1)][(node.y - 1) + 1] in stack)):
-      eastSquare = createNodeWithChilds(mazeArray[(node.x - 1)][(node.y - 1) + 1], nodeDepth, nodeCostSoFar)
+    if(not(mazeArray[(node.x - 1) + 1][(node.y - 1)] in stack)):
+      eastSquare = createNodeWithChilds(mazeArray[(node.x - 1) + 1][(node.y - 1)], nodeDepth, nodeCostSoFar)
       nodeChildren.append(eastSquare)
   if(not(node.hasSouthWall)):
-    if(not(mazeArray[(node.x - 1) + 1][(node.y - 1)] in stack)):
-      southSquare = createNodeWithChilds(mazeArray[(node.x - 1) + 1][(node.y - 1)], nodeDepth, nodeCostSoFar)
+    if(not(mazeArray[(node.x - 1)][(node.y - 1) + 1] in stack)):
+      southSquare = createNodeWithChilds(mazeArray[(node.x - 1)][(node.y - 1) + 1], nodeDepth, nodeCostSoFar)
       nodeChildren.append(southSquare)
   if(not(node.hasWestWall)):
-    if(not(mazeArray[(node.x - 1)][(node.y - 1) - 1] in stack)):
-      westSquare = createNodeWithChilds(mazeArray[(node.x - 1)][(node.y - 1) - 1], nodeDepth, nodeCostSoFar)
+    if(not(mazeArray[(node.x - 1) - 1][(node.y - 1)] in stack)):
+      westSquare = createNodeWithChilds(mazeArray[(node.x - 1) - 1][(node.y - 1)], nodeDepth, nodeCostSoFar)
       nodeChildren.append(westSquare)    
   if(not(node.hasNorthWall)):
-    if(not(mazeArray[(node.x - 1) - 1][(node.y - 1)] in stack)):
-      northSquare = createNodeWithChilds(mazeArray[(node.x - 1) - 1][(node.y - 1)], nodeDepth, nodeCostSoFar)
+    if(not(mazeArray[(node.x - 1)][(node.y - 1) - 1] in stack)):
+      northSquare = createNodeWithChilds(mazeArray[(node.x - 1)][(node.y - 1) - 1], nodeDepth, nodeCostSoFar)
       nodeChildren.append(northSquare)
   stack.pop()
   return Node(node, COST[node.squareType], False, nodeChildren, nodeDepth, costSoFar)
